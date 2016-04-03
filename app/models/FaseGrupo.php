761b00be2f81ce5c7df1352e4a2b88e6c2eda372
Hugo Rabelo <hleorabelo@gmail.com>
2016-04-03 20:06:02 -0300

    Tentando fechar issue #2

--- app/models/FaseGrupo.php
+++ app/models/FaseGrupo.php
@@ -9,6 +9,10 @@ class FaseGrupo extends Eloquent {
         'campeonato_fases_id' => 'required'
     );
 
+    public function fase() {
+        return CampeonatoFase::find($this->campeonato_fases_id);
+    }
+
     public function usuarios() {
         return $this->belongsToMany('User', 'usuario_grupos', 'fase_grupos_id', 'users_id')->withPivot(array('pontuacao'))->orderBy('pontuacao', 'desc')->getResults();
     }
@@ -16,6 +20,17 @@ class FaseGrupo extends Eloquent {
     public function usuariosComClassificacao() {
         $usuarios = $this->belongsToMany('User', 'usuario_grupos', 'fase_grupos_id', 'users_id')->getResults();
         $partidas = $this->partidas();
+        $pontuacoes = $this->fase()->pontuacoes();
+        Log::info($pontuacoes);
+        Log::info($partidas);
+
+        /*
+         * Para cada partida, trazer os usuários da partida;
+         * Para cada usuário, associar com um usuário da coleção principal;
+         * Verificar a pontuação do usuário.
+         */
+
+
         foreach($partidas as $partida) {
             foreach($usuarios as $usuario) {
                 //pegar a classificacao dos usuarios
@@ -27,17 +42,25 @@ class FaseGrupo extends Eloquent {
                 $num_gols_pro = 20;
                 $num_gols_contra = 8;
 
-                foreach($partida->usuarios as $usuarioPartida) {
-                    if($usuarioPartida->id == $usuario->id) {
+                $pontuacao = 0;
 
+                foreach($partida->usuarios as $usuarioPartida) {
+                    if($usuarioPartida->users_id == $usuario->id) {
+                        if($usuarioPartida->posicao != null) {
+                            $pontuacao = $pontuacoes[$usuarioPartida->posicao];
+                            if($usuario->id == 1) {
+                                Log::info($usuarioPartida);
+                            }
+                        }
+                        break;
                     }
                 }
 
                 $num_jogos = $num_vitorias + $num_empates + $num_derrotas;
-                $pontuacao = ($num_vitorias * 3) + $num_empates;
+                //$pontuacao = ($num_vitorias * 3) + $num_empates;
                 $num_saldo_gols = $num_gols_pro - $num_gols_contra;
 
-                $usuario->pontuacao = $pontuacao;
+                $usuario->pontuacao += $pontuacao;
                 $usuario->jogos = $num_jogos;
                 $usuario->vitorias = $num_vitorias;
                 $usuario->empates = $num_empates;
