5235ee2abbecbbbf0848743c9a85588349a64969
hugo.rabelo <hugo.rabelo@dataprev.gov.br>
2016-09-13 11:43:32 -0300

    Arquivo do build do gulp fazendo a limpeza correta do diretório path.dist

--- gulp/build.js
+++ gulp/build.js
@@ -108,7 +108,7 @@ gulp.task('other', function () {
 });
 
 gulp.task('clean', function () {
-    return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
+    return $.del([path.join(conf.paths.dist, '/app/'), path.join(conf.paths.dist, '/assets/'), path.join(conf.paths.dist, '/fonts/'), path.join(conf.paths.dist, '/maps/'), path.join(conf.paths.dist, '/scripts/'), path.join(conf.paths.dist, '/styles/'), path.join(conf.paths.tmp, '/')]);
 });
 
 gulp.task('build', ['html', 'fonts', 'other']);
