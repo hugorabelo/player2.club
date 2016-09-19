29533e130bb3ae95e68bdce02061301292b8a6e4
hugo.rabelo <hugo.rabelo@dataprev.gov.br>
2016-09-19 18:08:58 -0300

    Atualização do Layout com Material Design

--- src/app/components/comum/TopNavController.js
+++ src/app/components/comum/TopNavController.js
@@ -6,6 +6,13 @@
 
         var vm = this;
 
+        var originatorEv;
+
+        vm.openMenu = function ($mdOpenMenu, ev) {
+            originatorEv = ev;
+            $mdOpenMenu(ev);
+        };
+
         vm.mudaIdioma = function (idioma) {
             $translate.use(idioma);
 
