68bca264ce7325ef3a4b949c3e28cc95f10079cd
hugo.rabelo <hugo.rabelo@dataprev.gov.br>
2017-04-25 18:49:07 -0300

    Issue #139. Cadastro de Modelos de Campeonatos

---
+++ src/app/components/cadastroModeloCampeonato/ModeloCampeonatoFactory.js
@@ -0,0 +1,38 @@
+angular.module('player2').factory('ModeloCampeonato', ['$http', function ($http) {
+    return {
+        get: function () {
+            return $http.get('api/modeloCampeonato');
+        },
+
+        save: function (modelo) {
+            return $http({
+                method: 'POST',
+                url: 'api/modeloCampeonato',
+                data: $.param(modelo),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+        },
+
+        edit: function (id) {
+            return $http.get('api/modeloCampeonato/' + id + '/edit');
+        },
+
+        update: function (modelo) {
+            return $http({
+                method: 'PUT',
+                url: 'api/modeloCampeonato',
+                data: $.param(modelo),
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
+        },
+
+        destroy: function (id) {
+            return $http.delete('api/modeloCampeonato/' + id);
+        }
+
+    }
+}])
