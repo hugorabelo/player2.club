f2169b6944bbf03dbebbc96bfe4fdd2bd8070a3e
hugorabelo <hleorabelo@gmail.com>
2017-08-09 08:40:05 -0300

    Ajustes javascript para ficar ok com o JSLint

--- src/app/components/equipe/EquipeController.js
+++ src/app/components/equipe/EquipeController.js
@@ -9,7 +9,7 @@
             vm.equipe = {};
 
             vm.idEquipe = $stateParams.idEquipe;
-            if (vm.idEquipe != undefined) {
+            if (vm.idEquipe !== undefined) {
                 Equipe.show(vm.idEquipe)
                     .success(function (data) {
                         vm.equipe = data;
@@ -21,7 +21,29 @@
                     .success(function (data) {
                         vm.equipesDoUsuario = data;
                     })
-                    .error(function (error) {})
+                    .error(function (error) {});
+            };
+
+            function DialogController($scope, $mdDialog, tituloModal, novoItem, equipe) {
+                $scope.tituloModal = tituloModal;
+                $scope.novoItem = novoItem;
+                $scope.equipe = equipe;
+
+                $scope.cancel = function () {
+                    $mdDialog.cancel();
+                };
+
+                $scope.save = function () {
+                    vm.save($scope.equipe, $scope.files[0]);
+                    $mdDialog.hide();
+                };
+
+                $scope.update = function () {
+                    vm.update($scope.equipe, $scope.files[0]);
+                    $mdDialog.hide();
+                };
+
+                $scope.$watch('files.length', function (newVal, oldVal) {});
             }
 
             vm.create = function (ev) {
@@ -98,32 +120,27 @@
                     });
             };
 
-            function DialogController($scope, $mdDialog, tituloModal, novoItem, equipe) {
+            function DialogControllerMensagem($scope, $mdDialog, tituloModal, novaMensagem, nomeEquipe) {
                 $scope.tituloModal = tituloModal;
-                $scope.novoItem = novoItem;
-                $scope.equipe = equipe;
+                $scope.novaMensagem = novaMensagem;
+                $scope.nomeDestinatario = nomeEquipe;
 
                 $scope.cancel = function () {
                     $mdDialog.cancel();
                 };
 
-                $scope.save = function () {
-                    vm.save($scope.equipe, $scope.files[0]);
+                $scope.enviarMensagem = function () {
+                    vm.enviarMensagemEquipe(novaMensagem);
                     $mdDialog.hide();
                 };
 
-                $scope.update = function () {
-                    vm.update($scope.equipe, $scope.files[0]);
-                    $mdDialog.hide();
-                };
-
-                $scope.$watch('files.length', function (newVal, oldVal) {});
             }
 
             vm.escreverMensagem = function (ev) {
                 vm.novaMensagem = {};
                 vm.novaMensagem.id_equipe = vm.equipe.id;
-                $mdDialog.show({
+                $mdDialog
+                    .show({
                         locals: {
                             tituloModal: 'messages.escrever_mensagem',
                             novaMensagem: vm.novaMensagem,
@@ -141,22 +158,6 @@
                     }, function () {
 
                     });
-            };
-
-            function DialogControllerMensagem($scope, $mdDialog, tituloModal, novaMensagem, nomeEquipe) {
-                $scope.tituloModal = tituloModal;
-                $scope.novaMensagem = novaMensagem;
-                $scope.nomeDestinatario = nomeEquipe;
-
-                $scope.cancel = function () {
-                    $mdDialog.cancel();
-                };
-
-                $scope.enviarMensagem = function () {
-                    vm.enviarMensagemEquipe(novaMensagem);
-                    $mdDialog.hide();
-                }
-
             };
 
             vm.enviarMensagemEquipe = function (novaMensagem) {
@@ -182,7 +183,7 @@
                     $rootScope.loading = true;
                     Equipe.destroy(vm.equipe.id)
                         .success(function (data) {
-                            toastr.success($filter('translate')('messages.exclusao_equipe_sucesso'))
+                            toastr.success($filter('translate')('messages.exclusao_equipe_sucesso'));
                             $location.path('/home/equipes');
                         }).error(function (data, status) {
                             toastr.error($filter('translate')(data.errors), $filter('translate')('messages.exclusao_equipe_erro'));
@@ -192,6 +193,6 @@
 
                 });
             };
-    }]);
+        }]);
 
 }());
